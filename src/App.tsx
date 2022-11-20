6230b85684579c4aaadc185d95968fb7c842c501
TroyMaeder <troy.maeder@gmail.com>
2022-11-20 17:26:49 +0000

    Remove query logic from `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import { useQuery } from "@apollo/client";
-import Table from "./components/Table/Table";
-import { REPOSITORIES_QUERY } from "./queries/repositoriesQuery";
+import HomePage from "./pages/HomePage/HomePage";
 
-const App = () => {
-  const { data } = useQuery(REPOSITORIES_QUERY);
-  return (
-    <>
-      <Table data={data?.viewer.repositories.nodes} />
-    </>
-  );
-};
+const App = () => <HomePage />;
 
 export default App;
