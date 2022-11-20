d12aaa22622533bae2efafbe553bdac357e926e8
TroyMaeder <troy.maeder@gmail.com>
2022-11-20 19:31:07 +0000

    Remove loading state test from `renders page data`

--- src/pages/HomePage/__tests__/HomePage.test.tsx
+++ src/pages/HomePage/__tests__/HomePage.test.tsx
@@ -36,11 +36,11 @@ describe("home screen", () => {
       </MockedProvider>
     );
 
-    expect(await screen.findByText("Loading...")).toBeInTheDocument();
     expect(await screen.findByText("JavaScript")).toBeInTheDocument();
     expect(await screen.findByText("123")).toBeInTheDocument();
     expect(await screen.findByText("678")).toBeInTheDocument();
   });
+
   it("renders loading state", async () => {
     render(
       <MockedProvider mocks={repositoriesQueryMockResponse}>
