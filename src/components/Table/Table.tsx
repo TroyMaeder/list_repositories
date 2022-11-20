7257e5aa9f132dbbbff8db9a08847b9bc381f4f5
TroyMaeder <troy.maeder@gmail.com>
2022-11-20 17:35:15 +0000

    Add `Table` styling and accessibility

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,30 +1,43 @@
 import { FC } from "react";
 import { default as MUITable } from "@mui/material/Table";
 import { TableBody } from "@mui/material";
+import { styled } from "@mui/system";
 import { TableContainer } from "@mui/material";
 import { TableRow } from "@mui/material";
-import { TableCell } from "@mui/material";
-import { Paper } from "@mui/material";
 import { createRow, Row } from "../../helpers/createRow";
 
 interface Props {
   data: Row[] | undefined;
 }
 
+const TableCell = styled("td")({
+  width: "50px",
+  display: "table-cell",
+  textAlign: "center",
+  padding: "16px 4px",
+});
+
 const Table: FC<Props> = ({ data }) => {
   const rows = data?.map((row) => createRow(row));
 
   return (
     <>
-      <TableContainer component={Paper}>
+      <TableContainer>
         <MUITable>
           <TableBody>
             {rows?.map((row) => (
               <TableRow key={row.name}>
+                <TableCell aria-hidden="true"> - </TableCell>
                 <TableCell>{row.name}</TableCell>
-                <TableCell>{row.starIcon}</TableCell>
+                <TableCell aria-hidden="true"> - </TableCell>
+                <TableCell role="img" aria-label="star">
+                  {row.starIcon}
+                </TableCell>
                 <TableCell>{row.stargazerCount}</TableCell>
-                <TableCell>{row.forkIcon}</TableCell>
+                <TableCell aria-hidden="true"> - </TableCell>
+                <TableCell role="img" aria-label="fork">
+                  {row.forkIcon}
+                </TableCell>
                 <TableCell>{row.forkCount}</TableCell>
               </TableRow>
             ))}
